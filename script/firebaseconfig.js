3440118dd747e81823cbd3c1dc8819448bc8fc64
Dunderblixt <stalhane@gmail.com>
2026-03-19 13:40:18 +0100

    moved firebaseinit from index to js

--- script/firebaseconfig.js
+++ script/firebaseconfig.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getDatabase, ref, set, push, get } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyASUiN6n-p9_B9Ruox6l3ZmW6qbQx3kRgY",
+    authDomain: "flaskpost-8adcc.firebaseapp.com",
+    projectId: "flaskpost-8adcc",
+    storageBucket: "flaskpost-8adcc.firebasestorage.app",
+    messagingSenderId: "75468522109",
+    appId: "1:75468522109:web:6a69184654f1cea857e714"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 const baseUrl = "https://flaskpost-8eeb9-default-rtdb.europe-west1.firebasedatabase.app/users";
 
 //  Hämta alla users 
@@ -92,7 +107,6 @@ postBtn.addEventListener("click", async () => {
 
 // Logga in med google genom firebase / Henrik
 
-const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
